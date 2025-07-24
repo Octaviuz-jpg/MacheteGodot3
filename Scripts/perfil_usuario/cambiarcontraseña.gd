0693c083954ca8b07557e2575cca42785092ff6c
Maria Navarro <mariadvnr04@gmail.com>
2025-07-23 23:02:35 -0400

    Cambiar Contraseña

--- Scripts/perfil_usuario/cambiarcontraseña.gd
+++ Scripts/perfil_usuario/cambiarcontraseña.gd
@@ -2,5 +2,20 @@ extends Button
 
 
 func _on_Cambiar_contrasea_pressed():
-	print("Botón 'Cambiar Contraseña' presionado.")
-	pass 
+	var new_scene_path = "res://Scenes/CambiarContr.tscn"
+	var new_scene_packed = load(new_scene_path)
+	var new_scene_instance = new_scene_packed.instance()
+
+	# 2. Obtener la escena actual (la que queremos liberar)
+	var current_scene_node = get_tree().current_scene
+
+	# 3. Eliminar la escena actual del árbol
+	# Esto activará _exit_tree() en los nodos de la escena anterior
+	current_scene_node.queue_free()
+
+	# 4. Añadir la nueva escena al nodo raíz del árbol
+	# get_tree().root es el Viewport global del juego.
+	get_tree().root.add_child(new_scene_instance)
+
+	# 5. Establecer la nueva escena como la escena principal del árbol
+	get_tree().current_scene = new_scene_instance
